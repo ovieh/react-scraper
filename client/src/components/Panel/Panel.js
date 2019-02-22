6def557097a14419bf6646c52498201671bbd681
ovieh <ovieh.mosley@gmail.com>
2019-02-22 15:36:41 +0000

    added fallback element

--- client/src/components/Panel/Panel.js
+++ client/src/components/Panel/Panel.js
@@ -3,6 +3,10 @@ import { Card, CardBody, CardHeader, ListGroup, ListGroupItem, ListGroupItemHead
 
 const Panel = ({ articles }) => {
 
+  const parseArticles = (article, id) => {
+
+  }
+
   const renderArticles = articles => {
     if(articles && articles.length > 0)
       return articles.map((article, index) => <ListGroupItem key={index}>
@@ -11,6 +15,10 @@ const Panel = ({ articles }) => {
         </span>
         <ListGroupItemText>{`${article.summary}`}</ListGroupItemText>
       </ListGroupItem>)
+    else
+      return <ListGroupItem>
+        No Scraped Articles!
+      </ListGroupItem>
   }
 
   return(

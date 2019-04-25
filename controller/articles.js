f75d8e512988d7eea4fa79d7d8efcb3198a5b675
ovieh <ovieh.mosley@gmail.com>
2019-04-25 08:17:57 +0100

    added unsave

--- controller/articles.js
+++ controller/articles.js
@@ -1,48 +1,43 @@
 const Article = require('../models/article');
 
-
-// const setSaved = (id, saved) => {
-// 	return Article.findByIdAndUpdate(
-// 		id, {
-// 			saved
-// 		}, {
-// 			new: true
-// 		}
-// 	);
-// };
-
 module.exports = {
   find: function(req,res) {
     Article.find({})
     .then(articles => res.json(articles))
     .catch(err => res.status(500).json(err));
   },
+
   findSaved: function(req, res) {
     Article.find({ saved: true})
       .populate('comments')
       .then( dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
+
   save: function(req, res) {
-    Article.find({
-      saved: true
-    })
-    .then(articles => res.json(articles))
-    .catch(err => res.status(500).send(err))
+    // console.log(req.params.id)
+    setSaved(req.body.id, true)
+      .then(article => res.json(article))
+      .catch(err => res.status(404).json(err))
   },
-  setSaved: function(req, res) {
+
+  unsave: function(req, res) {
     Article.findByIdAndUpdate(
-      id, {
-        saved
-      }, {
-        new: true
-      }
+      req.body._id, { saved: 'false'}
     )
-  },
-  unsave: function(req,res) {
-    setSaved(req.body.id, false)
     .then(article => res.json(article))
-    .catch(err => res.status(404).json(err))
-  }
+    .catch(err => res.status(422).json(err))
+  },
+  
+  setSaved: function(req, res) {
+    // console.log(req.body._id)
+    Article.findByIdAndUpdate(
+      req.body._id, { saved: 'true' }
+    )
+    .then(article => res.json(article))
+    .catch(err => res.status(422).json(err))
+  },
 
 }
+
+
